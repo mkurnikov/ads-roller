29684d9f6470f5f4121b19944e5a8e557e5201ee
Maxim Kurnikov <maxim.kurnikov@gmail.com>
2019-12-23 17:16:17 +0300

    cleanup a bit, add more tests

--- src/ads.rs
+++ src/ads.rs
@@ -1,68 +1,98 @@
-use rand::distributions::WeightedIndex;
+use rand::distributions::{Distribution, WeightedIndex};
+use rand::thread_rng;
 
 use crate::models::Ad;
 
-pub fn get_standard_probability(total: usize) -> f32 {
+fn get_standard_probability(total: usize) -> f32 {
     1.0 / total as f32
 }
 
-pub fn compute_ad_probability(ad: &Ad, total_num_ads: usize) -> f32 {
+fn compute_ad_probability(ad: &Ad, total_num_ads: usize, is_last_ad: bool) -> f32 {
     let standard_ad_probability = get_standard_probability(total_num_ads);
+    if is_last_ad {
+        return standard_ad_probability * 0.01;
+    }
     if ad.categories.is_empty() {
         return standard_ad_probability;
     }
     standard_ad_probability * (1.0 - 0.1 * ad.categories.len() as f32)
 }
 
-pub fn get_distribution(ads: &[Ad], total_ads: usize, last_ad: Option<Ad>) -> WeightedIndex<f32> {
+fn get_distribution(ads: &[Ad], total_ads: usize, last_ad: Option<Ad>) -> WeightedIndex<f32> {
     let probs: Vec<f32> = ads
         .iter()
-        .map(|ad| {
-            if let Some(last_ad) = &last_ad {
-                if ad == last_ad {
-                    return get_standard_probability(total_ads) * 0.01;
-                }
-            }
-            compute_ad_probability(ad, total_ads)
+        .map(|ad| match last_ad {
+            Some(ref last_ad) => compute_ad_probability(ad, total_ads, ad == last_ad),
+            None => compute_ad_probability(ad, total_ads, false),
         })
         .collect();
 
     WeightedIndex::new(probs).unwrap()
 }
 
+pub fn get_sampled_ad(ads: &[Ad], total_ads: usize, last_ad: Option<Ad>) -> &Ad {
+    let dist = get_distribution(&ads, total_ads, last_ad);
+    let mut rng = thread_rng();
+
+    let ad_index = dist.sample(&mut rng);
+    ads.get(ad_index).unwrap()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn make_an_ad(categories: Vec<String>) -> Ad {
+        Ad {
+            id: 1,
+            url: String::from("http://localhost/"),
+            num_prepaid_shows: 1,
+            categories,
+        }
+    }
+
     #[test]
     fn test_probability_no_categories() {
-        let ad = Ad {
-            id: 0,
-            url: "http://localhost".to_string(),
-            num_prepaid_shows: 10,
-            categories: vec![],
-        };
-        assert_eq!(compute_ad_probability(&ad, 1), 1.0);
-        assert_eq!(compute_ad_probability(&ad, 2), 0.5);
-        assert_eq!(compute_ad_probability(&ad, 4), 0.25);
+        let ad = make_an_ad(vec![]);
+        assert_eq!(compute_ad_probability(&ad, 1, false), 1.0);
+        assert_eq!(compute_ad_probability(&ad, 2, false), 0.5);
+        assert_eq!(compute_ad_probability(&ad, 4, false), 0.25);
     }
 
     #[test]
     fn test_if_banner_has_more_categories_less_probability() {
-        let ad1 = Ad {
-            id: 0,
-            url: "http://localhost".to_string(),
-            num_prepaid_shows: 10,
-            categories: vec![],
-        };
-        let ad2 = Ad {
-            id: 0,
-            url: "http://localhost".to_string(),
-            num_prepaid_shows: 10,
-            categories: vec!["hello".to_string(), "world".to_string()],
-        };
+        let ad1 = make_an_ad(vec![]);
+        let ad2 = make_an_ad(vec!["hello".to_string()]);
 
         let total = 10;
-        assert!(compute_ad_probability(&ad1, total) > compute_ad_probability(&ad2, total));
+        assert!(
+            compute_ad_probability(&ad1, total, false) > compute_ad_probability(&ad2, total, false)
+        );
+    }
+
+    #[test]
+    fn test_if_last_ad_do_not_sample() {
+        // without is_last_ad, prob will be lower
+        let ad1 = make_an_ad(vec![
+            "hello".to_string(),
+            "hello2".to_string(),
+            "hello3".to_string(),
+        ]);
+        let ad2 = make_an_ad(vec![]);
+
+        let total = 10;
+        assert!(
+            compute_ad_probability(&ad1, total, false) > compute_ad_probability(&ad2, total, true)
+        )
+    }
+
+    #[test]
+    fn test_sample_an_ad() {
+        let ads = vec![make_an_ad(vec![]), make_an_ad(vec![])];
+        let ad = get_sampled_ad(&ads, 2, None);
+
+        assert!(ads.contains(ad));
+        assert!(ads.contains(ad));
+        assert!(ads.contains(ad));
     }
 }
--- src/bin/web.rs
+++ src/bin/web.rs
@@ -3,13 +3,10 @@
 #[macro_use]
 extern crate rocket;
 
-use rand::distributions::Distribution;
-use rand::thread_rng;
 use rocket::http::RawStr;
 use serde::Serialize;
 
-use ads_roller::ads::get_distribution;
-use ads_roller::db::{count_ads, decrement_ad_prepaid_shows, get_connection, select_matching_ads};
+use ads_roller::{ads, db};
 
 #[derive(Serialize)]
 struct AdUrl {
@@ -27,17 +24,13 @@ fn fetch_ad(
         .filter(|cat| cat.is_some())
         .map(|cat| cat.unwrap().to_string())
         .collect();
-    let conn = get_connection();
+    let conn = db::get_connection();
 
-    let ads = select_matching_ads(selected_categories, &conn);
-    let total_ads = count_ads(&conn);
+    let ads = db::select_matching_ads(selected_categories, &conn);
+    let total_ads = db::count_ads(&conn);
 
-    let dist = get_distribution(&ads, total_ads, None);
-    let mut rng = thread_rng();
-
-    let ad_index = dist.sample(&mut rng);
-    let selected_ad = ads.get(ad_index).unwrap();
-    decrement_ad_prepaid_shows(selected_ad, &conn);
+    let selected_ad = ads::get_sampled_ad(&ads, total_ads, None);
+    db::decrement_ad_prepaid_shows(selected_ad, &conn);
 
     let ad_url = AdUrl {
         url: selected_ad.url.clone(),
